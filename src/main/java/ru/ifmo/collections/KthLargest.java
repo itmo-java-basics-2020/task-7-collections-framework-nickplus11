92d88e887d7d44fb74ff3f2907c8b942578107de
nickplus11 <nick_plus@mail.ru>
2020-05-19 19:24:14 +0300

    Solution for Task7

--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,6 +1,5 @@
 package ru.ifmo.collections;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -11,40 +10,22 @@ import java.util.PriorityQueue;
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
 public class KthLargest {
-    PriorityQueue<Integer> priorityQueue;
-    int k;
+    private final PriorityQueue<Integer> priorityQueue;
+    private final int k;
 
     public KthLargest(int k, int[] numbers) {
-        if (numbers == null) {
-            throw new IllegalArgumentException();
-        }
-
         this.k = k;
-        priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if (o1 > o2) {
-                    return -1;
-                }
-                if (o1 < o2) {
-                    return 1;
-                }
-                return 0;
-            }
-        });
-
-        for (var n : numbers) {
-            priorityQueue.add(n);
-        }
-
-        while (priorityQueue.size() > k) {
-            priorityQueue.remove();
+        priorityQueue = new PriorityQueue<>();
+        for (int e : numbers) {
+            add(e);
         }
     }
 
     public int add(int val) {
         priorityQueue.add(val);
-        priorityQueue.remove();
+        if (priorityQueue.size() > k) {
+            priorityQueue.remove();
+        }
         return priorityQueue.element();
     }
 }
--- src/test/java/ru/ifmo/collections/KthLargestTest.java
+++ src/test/java/ru/ifmo/collections/KthLargestTest.java
@@ -10,9 +10,9 @@ public class KthLargestTest {
     public void kthLargestTest() {
         KthLargest kthLargest = new KthLargest(3, new int[]{4, 5, 8, 2});
         assertThat(kthLargest.add(3), is(4));
-        assertThat(kthLargest.add(5), is(4));
-        assertThat(kthLargest.add(10), is(4));
-        assertThat(kthLargest.add(9), is(4));
-        assertThat(kthLargest.add(4), is(4));
+        assertThat(kthLargest.add(5), is(5));
+        assertThat(kthLargest.add(10), is(5));
+        assertThat(kthLargest.add(9), is(8));
+        assertThat(kthLargest.add(4), is(8));
     }
 }
